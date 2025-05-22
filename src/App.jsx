de1b9b2dc301dbda7b54d86cfe5402020bb50017
GusWiraAditya <mrxjojo25@gmail.com>
2025-05-23 03:23:20 +0800

    error import navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Technologies from "./sections/Technologies";
